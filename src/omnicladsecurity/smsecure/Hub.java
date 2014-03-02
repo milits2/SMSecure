8ad5a96b5c13f8893b5e86b53039966943b9380a
fuchuzz <fuchuzz@aol.com>
2014-03-02 13:57:25 -0500

    Send SMS in app
    PoC of sending an SMS text message in the application, also added a new
    screen.

--- src/omnicladsecurity/smsecure/Hub.java
+++ src/omnicladsecurity/smsecure/Hub.java
@@ -6,6 +6,7 @@ import omnicladsecurity.smsecure.OneTimePad;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.telephony.SmsManager;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
@@ -27,6 +28,29 @@ public class Hub extends Activity {
         return true;
     }
 
+    public void sendMessageButtonClick(View view)
+    {
+    	TextView phoneNumber = (TextView)findViewById(R.id.phoneNumberTextBox);
+    	TextView message = (TextView)findViewById(R.id.messageTextBox);
+    	
+    	sendTextMessage(message.getText().toString(), phoneNumber.getText().toString());
+    	   	
+    }
+    
+    public void sendTextMessage(String text, String address)
+    {
+    	SmsManager smsManager = SmsManager.getDefault();
+    	smsManager.sendTextMessage(address, null, text, null, null);
+    }
+    
+    public void messageScreen(View view) {
+    	setContentView(R.layout.message);
+    }
+    
+    public void hubScreen(View view) {
+    	setContentView(R.layout.activity_hub);
+    }
+    
     public void generateOneTimePad(View view) {
     	TextView padDisplay = (TextView)findViewById(R.id.padContents);
     	pad = new OneTimePad(1024);
