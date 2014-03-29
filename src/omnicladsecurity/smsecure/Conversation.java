cf2f205c9c4cf53022c55e3941f7e843eb2091ec
Samuel Milito <milits2rpi@gmail.com>
2014-03-29 14:38:01 -0400

    Created conversation class with base read/send
    Created a conversation class to more properly encapsulate conversation
    behavior. Right now it just covers sending/reading messages.

---
+++ src/omnicladsecurity/smsecure/Conversation.java
@@ -0,0 +1,44 @@
+package omnicladsecurity.smsecure;
+
+public class Conversation {
+	// The Conversation class contains the pertinent information for a conversation.
+	// They are loaded upon opening a conversation and populate themselves.
+	OneTimePad localPad, contactPad;
+	String contactNumber;
+	
+	public Conversation(String withNumber) {
+		contactNumber = withNumber;
+		
+		// Load the one-time pads from memory
+		// TODO
+	}
+	
+	public String readMessage(String message) {
+		// Encrypted messages will be of the form |~|offset|message
+		if(message.startsWith("|~|")) {
+			// Find the offset.
+			int offset;
+			String[] components = message.split("|");
+			if(components.length != 3) {
+				return "";
+			}
+			try {
+				offset = Integer.parseInt(components[1]);
+			} catch(NumberFormatException e) {
+				return "";
+			}
+			
+			// Decrypt the message.
+			return contactPad.decrypt(components[2], offset);
+		}
+		// If it doesn't have the prefix, skip the message.
+		return "";
+	}
+	
+	public String sendMessage(String message) {
+		// Encrypted messages will be of the form |~|offset|message
+		String prefix = "|~|" + localPad.offset() + "|";
+		String suffix = localPad.encrypt(message, localPad.offset());
+		return prefix + suffix;
+	}
+}
