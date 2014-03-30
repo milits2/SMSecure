793fdbc3be6213b0745095aaa0dc5488035996b0
geralddarko <geralddarko21@gmail.com>
2014-03-30 01:58:56 -0400

    Saving to internal memory
    function to save an array of strings to a file
    function to save a pad and an offset integer to files in a folder
    corresponding to the number they belong to

--- src/omnicladsecurity/smsecure/MessageHandler.java
+++ src/omnicladsecurity/smsecure/MessageHandler.java
@@ -1,6 +1,12 @@
 package omnicladsecurity.smsecure;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 import android.content.Context;
+import android.util.Log;
 
 public class MessageHandler {
 	OneTimePad localPad, contactPad;
@@ -65,4 +71,67 @@ public class MessageHandler {
 		// If it doesn't have the prefix, skip the message.
 		return message;
 	}
+	
+
+public void storeNumbers(String[] numbers) {
+	String filename = "conversations.txt";
+	File numberList = new File(context.getFilesDir(), filename);
+
+
+	try {
+    	OutputStream os = new FileOutputStream(numberList);
+		for(String number: numbers) {
+			os.write(number.getBytes());
+		}
+        os.close();
+	}    catch (IOException e) {
+        // Unable to create file for unknown reasons, seriously I'm doing
+		//like the exact same thing as jake argh
+		//except context, apparently he didn't need to do that because external memory
+        Log.w("ExternalStorage", "Error writing " + numberList, e);
+   	}
+  }
+
+public void storeOffest(String number, int offset) {
+	String filename = "pad.txt";
+	File path = new File(context.getFilesDir()+"/"+number);
+	path.mkdir();  //make it and/or make sure it exists
+	File offsetFile = new File(context.getFilesDir()+"/"+number, filename);
+	String writeOffset = ( (Integer) offset).toString();
+	
+	try {
+    	OutputStream os = new FileOutputStream(offsetFile);
+    	os.write(writeOffset.getBytes());
+        os.close();
+	}    catch (IOException e) {
+        // Unable to create file
+        Log.w("ExternalStorage", "Error writing " + offsetFile, e);
+   	}
+  }
+
+public void storePad(String number) {
+	String filename = "pad.txt";
+	File path = new File(context.getFilesDir()+"/"+number);
+	path.mkdir();  //make it and/or make sure it exists
+	File padFile = new File(context.getFilesDir()+"/"+number, filename);
+	OneTimePad padData;
+	padData = new OneTimePad(1024);
+	
+	try {
+    	OutputStream os = new FileOutputStream(padFile);
+    	os.write(new String(padData.pad).getBytes());
+        os.close();
+	}    catch (IOException e) {
+        // Unable to create file
+        Log.w("ExternalStorage", "Error writing " + padFile, e);
+   	}
+  }
+
+public File retrievePad(String number) {
+	File path = new File(context.getFilesDir()+"/"+number);
+	File pad = new File(path, "pad.txt");
+	
+	return pad;
+  }
+
 }
